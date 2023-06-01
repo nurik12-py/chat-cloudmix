372859541e26533d670fa580472bc81ce407024b
nurik12-py <nurik.py.12@gmail.com>
2023-06-01 13:19:23 +0600

    fix: replace a with div

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
-import { CloudIcon, FireIcon } from "@heroicons/react/24/solid";
+import { CloudIcon } from "@heroicons/react/24/solid";
 import { Spin } from "antd";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
@@ -19,14 +18,10 @@ export default function Home() {
   return (
     <main className="w-full h-full flex flex-col items-center justify-center overflow-hidden">
       <CloudIcon className="w-10 h-10 text-indigo-500" />
-      <a
-        className="text-lg font-medium"
-        href="#"
-        style={{ fontFamily: "Inter" }}
-      >
+      <div className="text-lg font-medium" style={{ fontFamily: "Inter" }}>
         <span className="font-medium">Cloud</span>
         <span className="font-medium text-indigo-500">Mix</span>
-      </a>
+      </div>
       <Spin className="mt-2" />
     </main>
   );
