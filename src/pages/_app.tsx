0553ca8f289f1701d3bb49f4a682899b97da1c8d
nurik12-py <nurik.py.12@gmail.com>
2023-06-01 13:26:21 +0600

    refactor: rewrite to function component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,37 +1,32 @@
-import "antd/dist/reset.css";
-import "@/styles/globals.css";
-
-import App from "next/app";
 import React from "react";
+import { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { ConfigProvider } from "antd";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Head from "next/head";
+import "antd/dist/reset.css";
+import "@/styles/globals.css";
 
-class MyApp extends App {
-  queryClient = new QueryClient();
+function MyApp({ Component, pageProps }: AppProps & { Component: any }) {
+  const Layout = Component.Layout || React.Fragment;
+  const [queryClient] = React.useState(() => new QueryClient());
 
-  render() {
-    const { Component, pageProps } = this.props;
-    const Layout = Component.Layout || React.Fragment;
-
-    return (
-      <>
-        <Head>
-          <title>CloudMix</title>
-        </Head>
-        <RecoilRoot>
-          <ConfigProvider>
-            <QueryClientProvider client={this.queryClient}>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </QueryClientProvider>
-          </ConfigProvider>
-        </RecoilRoot>
-      </>
-    );
-  }
+  return (
+    <>
+      <Head>
+        <title>CloudMix</title>
+      </Head>
+      <RecoilRoot>
+        <ConfigProvider>
+          <QueryClientProvider client={queryClient}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </QueryClientProvider>
+        </ConfigProvider>
+      </RecoilRoot>
+    </>
+  );
 }
 
 export default MyApp;
